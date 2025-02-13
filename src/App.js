45f52e02a12d3e038d871299b844b0f31167787c
stampatron <stampatron1@gmail.com>
2025-02-13 20:50:42 +0000

    Initial boiler plate commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("/api/hello")
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+            .catch(error => console.error("Error fetching data:", error));
+    }, []);
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-primary">{message || "Loading..."}</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+    const isDev = argv.mode === 'development';
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        devtool: isDev ? 'source-map' : false,
+        devServer: {
+            static: './dist',
+            hot: true,
+            port: 8080,
+            historyApiFallback: true,
+            proxy: [
+                {
+                    context: ['/api'],
+                    target: "http://localhost:5000" // Redirect API calls to Express backend
+                }
+            ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/, 
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.scss$/,
+                    oneOf: [
+                        {
+                            // Rule for Bootstrap SCSS
+                            include: /node_modules\/bootstrap[\\/]scss/,
+                            use: [
+                                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                                'css-loader',
+                                {
+                                    loader: 'sass-loader',
+                                    options: {
+                                        sassOptions: {
+                                            quietDeps: true // Suppress only Bootstrap warnings
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            // Rule for all other SCSS
+                            exclude: /node_modules\/bootstrap[\\/]scss/,
+                            use: [
+                                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                                'css-loader',
+                                'sass-loader' // No quietDeps, so warnings show up
+                            ]
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/,
+                    type: 'asset/resource'
+                }          
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            })
+        ]
+    };
+};
