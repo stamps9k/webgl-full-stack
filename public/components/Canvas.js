864aeb20ac1f635962ec34070b0d970c8bdb1ee2
stampatron <stampatron1@gmail.com>
2025-02-17 20:17:42 +0000

    Updated site to use QueryString to set model & shaders

--- public/components/Canvas.js
+++ public/components/Canvas.js
@@ -26,7 +26,41 @@ const Canvas = () => {
 
     return (
         <div>
-            <canvas id="glCanvas" className="border" width="640" height="480"></canvas>
+            <h1>Web Assembly Model Viewer</h1>
+            <div class="card-header py-3" id="collapseHeading">
+                <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    Choose Model
+                </a>
+            </div>
+            <div id="collapseOne" class="collapse py-3">
+                <div class="card-body">
+                    <form id="model" target="_self" method="get" action="/index.html">
+                        <label for="model">Model: </label>
+                        <select id="model" name="model">
+                            <option value="cube">cube</option>
+                            <option value="cube-time">cube-time</option>
+                            <option value="cube-mouse">cube-mouse</option>
+                            <option value="teapot">teapot</option>
+                            <option value="cube-tex">textured cube</option>
+                        </select>
+                        <input type="submit" />
+                    </form>
+                </div>
+            </div>
+            <canvas id="glCanvas" class="border" width="640" height="480"></canvas>
+            <div class="row">
+                <div id="fps" class="w-25">
+                    FPS - #N/A
+                </div>
+                <div class="w-75">
+                    <label for="rotation_x"> Rotate X:</label>
+                    <input type="checkbox" id="rotation_x" name="rotation_x" value="rotation_x" onchange="update_rotate_x(this)" />
+                    <label for="rotation_y"> Rotate Y:</label>
+                    <input type="checkbox" id="rotation_y" name="rotation_y" value="rotation_y" onchange="update_rotate_y(this)" />
+                    <label for="rotation_z"> Rotate Z:</label>
+                    <input type="checkbox" id="rotation_z" name="rotation_z" value="rotation_z" onchange="update_rotate_z(this)" />
+                </div>
+            </div>
         </div>
     );
 };
