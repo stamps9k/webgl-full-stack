2eaca2032f7c2e9d43375de5d97bdefb4a1de60a
stampatron <stampatron1@gmail.com>
2025-02-21 11:05:49 +0000

    Added texture related files to the index.js for loading with webpack.

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ import App from "./components/App.js";
 
 //Assets imported by webpack
 import '../assets/models/cube.obj';
+import '../assets/models/cube-tex.obj';
 import '../assets/models/teapot.obj';
 import '../assets/img/icon.jpg';
 import '../assets/img/favicons/favicon.ico';
@@ -13,6 +14,9 @@ import '../assets/shaders/vert-color-mouse.vert';
 import '../assets/shaders/vert-color-mouse.frag';
 import '../assets/shaders/vert-color-time.vert';
 import '../assets/shaders/vert-color-time.frag';
+import '../assets/shaders/cube-tex.vert';
+import '../assets/shaders/cube-tex.frag';
+import '../assets/textures/cube-tex.tex';
 import "./css/cover.css";
 
 //React initialization
