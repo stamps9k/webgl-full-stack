ca798f92595715e7897873a5a39fe3e1c1843931
stampatron <stampatron1@gmail.com>
2025-08-19 14:18:54 +0900

    Removed commented out old code lines from Jest configuration file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  //setupFiles: ['<rootDir>/jest.setup.cjs'],
-  //setupFilesAfterEnv: ['<rootDir>/jest.setupAfterEnv.cjs'],
   roots: ['<rootDir>/tests'],
   transform: {
     "\\.[jt]sx?$": "babel-jest",
