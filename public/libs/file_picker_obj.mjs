86fe836122e7a4b9b9c1f570170c085354eee847
Stampatron <james@nitori.local>
2026-04-13 11:11:20 +0900

    Reverted large file upload limit for use in live.

--- public/libs/file_picker_obj.mjs
+++ public/libs/file_picker_obj.mjs
@@ -8,9 +8,9 @@ import { change_model } from "./webgl.mjs";
  */
 async function validate_obj(file) {
     //TMP debug. Allow large files
-    if (file.size > 1000485760) {
+    //if (file.size > 1000485760) {
     // Reject any files that are too big. 1048576 is 10MB in Bytes.
-    //if (file.size > 10485760) {
+    if (file.size > 10485760) {
         logger["error_js_opfs"]("Validation failed file is too large. Size is " + file.size)
         return { valid: false, error: 'File is too large. Maximum vald size is 10MB.' };
     }
